87d4023185a82b3166a56af7f0f48781f9fcd1ff
sapphiresao <saodat.kurmanayeva@nu.edu.kz>
2023-04-13 02:01:47 +0600

    1

--- d3.js
+++ d3.js
@@ -1,21 +1,23 @@
-const data = d3.range(100).map(() => {
-    return [Math.random() * 500, Math.random() * 500];
-  });
-  
-  const svg = d3.select("body")
-    .append("svg")
-    .attr("width", 500)
-    .attr("height", 500);
-  
-  svg.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", (d) => d[0])
-    .attr("cy", (d) => d[1])
-    .attr("r", 5)
-    .attr("fill", "black");
-  
+const width = 500;
+const height = 500;
+
+const svg = d3.select('svg');
+
+const data = d3.range(100).map(() => ({
+  x: Math.random() * width,
+  y: Math.random() * height,
+  r: Math.random() * 10 + 5,
+}));
+
+const circles = svg
+  .selectAll('circle')
+  .data(data)
+  .enter()
+  .append('circle')
+  .attr('cx', d => d.x)
+  .attr('cy', d => d.y)
+  .attr('r', d => d.r);
+
 d3.csv("titanic.csv").then((data) => {
   const ageData = d3.group(data, (d) => d.Age);
   const ageCounts = Array.from(ageData, ([age, passengers]) => ({ age: age, count: passengers.length }));
