183b1846790477099f335a58c398f1d87d552912
sapphiresao <saodat.kurmanayeva@nu.edu.kz>
2023-04-14 10:34:39 +0600

    python :D

--- d3.js
+++ d3.js
@@ -1,3 +1,4 @@
+function load () {
 const width = 500;
 const height = 500;
 
@@ -18,37 +19,81 @@ const circles = svg
   .attr('cy', d => d.y)
   .attr('r', d => d.r);
 
-d3.csv("titanic.csv").then((data) => {
-  const ageData = d3.group(data, (d) => d.Age);
-  const ageCounts = Array.from(ageData, ([age, passengers]) => ({ age: age, count: passengers.length }));
 
-  const pie = d3.pie()
-              .value((d) => d.count);
+      d3.csv("titanic.csv").then(function (data) {
+        // define the age categories
+        const ageCategories = [
+          { label: "0-10", min: 0, max: 10 },
+          { label: "10-20", min: 10, max: 20 },
+          { label: "20-30", min: 20, max: 30 },
+          { label: "30-40", min: 30, max: 40 },
+          { label: "40-50", min: 40, max: 50 },
+          { label: "50-60", min: 50, max: 60 },
+          { label: "60-70", min: 60, max: 70 },
+          { label: "70-80", min: 70, max: 80 },
+          { label: "80-90", min: 80, max: 90 },
+          { label: "90-100", min: 90, max: 100 },
+        ];
 
-  const arc = d3.arc()
-              .innerRadius(0)
-              .outerRadius(200);
+        // group the passengers by age category
+        const ageData = ageCategories.map((category) => {
+          const passengers = data.filter(
+            (passenger) =>
+              passenger.Age >= category.min && passenger.Age < category.max
+          );
+          return { category: category.label, count: passengers.length };
+        });
 
-  const color = d3.scaleOrdinal(d3.schemeCategory10);
+        // define the dimensions of the SVG container
+        const width = 400;
+        const height = 400;
+        const radius = Math.min(width, height) / 2;
 
-  const pieSvg = d3.select("body")
-                  .append("svg")
-                  .attr("width", 400)
-                  .attr("height", 400);
+        // define the color scale
+        const color = d3
+          .scaleOrdinal()
+          .domain(ageCategories.map((category) => category.label))
+          .range(d3.schemeSet2);
 
-  const arcs = pieSvg.selectAll("arc")
-                .data(pie(ageCounts))
-                .enter()
-                .append("g")
-                .attr("class", "arc")
-                .attr("transform", "translate(200,200)");
+        // define the pie generator
+        const pie = d3
+          .pie()
+          .value((d) => d.count)
+          .sort(null);
 
-  arcs.append("path")
-      .attr("d", arc)
-      .attr("fill", (d) => color(d.data.age));
+        // define the arc generator
+        const arc = d3
+          .arc()
+          .innerRadius(radius * 0.5)
+          .outerRadius(radius * 0.8);
 
-  arcs.append("text")
-      .attr("transform", (d) => `translate(${arc.centroid(d)})`)
-      .attr("text-anchor", "middle")
-      .text((d) => d.data.age);
-});
+        // create the SVG container
+        const svg = d3
+          .select("body")
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+          .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+        // create the pie chart
+        const arcs = svg
+          .selectAll("arc")
+          .data(pie(ageData))
+          .enter()
+          .append("g")
+          .attr("class", "arc");
+
+        arcs
+          .append("path")
+          .attr("d", arc)
+          .attr("fill", (d) => color(d.data.category));
+
+        // add labels to the pie chart
+        arcs
+          .append("text")
+          .attr("transform", (d) => `translate(${arc.centroid(d)})`)
+          .attr("text-anchor", "middle")
+          .text((d) => d.data.category);
+      });
+    }
